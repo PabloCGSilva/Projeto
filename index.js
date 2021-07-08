5a8667ffa532762d6784b3268cc4084feee87038
Pablo Silva <pablocarlos1982@gmail.com>
2021-07-08 19:59:33 -0300

    mongoose

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
+const ejs = require('ejs')
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    //    res.sendFile(path.join(__dirname + '/pages/index.html'));
+    res.render('index')
 })
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
