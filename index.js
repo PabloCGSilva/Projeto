67fb32be187e557d8452adb19bfc191a73692aed
Pablo Silva <pablocarlos1982@gmail.com>
2021-07-15 22:17:19 -0300

    erro em subir imagem

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ejs = require('ejs')
 const mongoose = require('mongoose')
 const { MongoClient } = require('mongodb')
 const blogPost = require('./models/blogPosts')
-
+const fileUpload = require('express-fileupload')
 
 mongoose.connect('mongodb://localhost/my_database')
 
@@ -26,6 +26,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(fileUpload())
 
 app.get('/', async(req, res) => {
     const blogposts = await blogPost.find({})
@@ -41,8 +42,9 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
-app.get('/post', (req, res) => {
-    res.render('post')
+app.get('/post/:id', async(req, res) => {
+    const blogposts = await blogPost.findById(req.params.id)
+    res.render('post', { blogposts })
 })
 
 app.get('/posts/new', (req, res) => {
@@ -50,10 +52,14 @@ app.get('/posts/new', (req, res) => {
 })
 
 app.post('/posts/store', async(req, res) => {
-    await blogPost.create(req.body);
-    res.redirect('/')
-})
+    let image = req.files.image
+    image.mv(path.resolve(__dirname, './public/img', image.name),
+        async(error) => {
+            await blogPost.create(req.body);
+            res.redirect('/')
+        })
 
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
